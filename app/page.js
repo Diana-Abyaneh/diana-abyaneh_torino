2d26f7ea576efef9c08acc8c585f618b49fa7b76
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-10-16 01:00:59 +0330

    refactor Home component to enhance image structure and styling

--- app/page.js
+++ app/page.js
@@ -6,15 +6,14 @@ import SearchForm from "@/components/forms/searchForm";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Image
-        src={travel}
-        alt="travel"
-        className={styles.image}
-        layout="responsive"
-        width={1440}
-        height={350}
-      />
-      <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src={travel}
+          alt="travel"
+          layout="responsive"
+        />
+      </div>
+      <div className={styles.form}>
         <h2>
           <span>تورینو</span> برگزار کننده بهترین تور های داخلی و خارجی
         </h2>
