a1927b251356bdf0b80dc54f92aca43bef5950f7
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-10-16 02:19:11 +0330

    refactor layout component for improved structure and styling

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,11 @@ import { IoPerson } from "react-icons/io5";
 import Image from "next/image";
 import torino from "../images/Torino.svg";
 import aira from "../images/aira.svg";
-import samandehi from "../images/samandehi.svg"
-import qrCode from "../images/ecunion.svg"
+import samandehi from "../images/samandehi.svg";
+import qrCode from "../images/ecunion.svg";
 import rights from "../images/passenger-rights.svg";
-import airline from "../images/state-airline.svg"
-import '../styles/globals.css'
+import airline from "../images/state-airline.svg";
+import "../styles/globals.css";
 
 export default function RootLayout({ children }) {
   return (
@@ -31,34 +31,34 @@ export default function RootLayout({ children }) {
           </header>
           {children}
           <footer>
-            <div>
+            <div className="footerContainer">
               <div>
-                <h3>تورینو</h3>
+                <h2>تورینو</h2>
                 <p>درباره ما</p>
                 <p>تماس با ما</p>
                 <p>چرا تورینو؟</p>
                 <p>بیمه مسافرتی</p>
               </div>
               <div>
-                <h3>خدمات مشتریان</h3>
+                <h2>خدمات مشتریان</h2>
                 <p>پشتیبانی آنلاین</p>
                 <p>راهنمای خرید</p>
                 <p>راهنمای استرداد</p>
                 <p>پرسش و پاسخ</p>
               </div>
-              <div>
-                <Image src={torino} alt="Torino logo"/>
-                <p>تلفن پشتیبانی: 021-8574</p>
-                <div>
-                  <Image src={aira} alt="aira.ir"/>
-                  <Image src={samandehi} alt="samandehi.ir"/>
-                  <Image src={qrCode} alt="ecunion"/>
-                  <Image src={rights} alt="passenger rights"/>
-                  <Image src={airline} alt="state airlines"/>
+              <div className="certificates">
+                <Image src={torino} alt="Torino logo" />
+                <p>تلفن پشتیبانی: 8574-021</p>
+                <div className="images">
+                  <Image src={aira} alt="aira.ir" />
+                  <Image src={samandehi} alt="samandehi.ir" />
+                  <Image src={qrCode} alt="ecunion" />
+                  <Image src={rights} alt="passenger rights" />
+                  <Image src={airline} alt="state airlines" />
                 </div>
               </div>
-              <p>کلیه حقوق این وبسایت متعلق به تورینو می باشد.</p>
             </div>
+            <p className="rights">کلیه حقوق این وبسایت متعلق به تورینو می باشد.</p>
           </footer>
         </ReactQueryProvider>
       </body>
