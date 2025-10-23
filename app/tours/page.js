65a1e5f030c484de10e3d3f4fb9107d464e46ee2
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-10-24 01:17:30 +0330

    fix: update price formatting to use Persian locale

--- app/tours/page.js
+++ app/tours/page.js
@@ -48,7 +48,7 @@ function Tours() {
                     <button>جزئیات</button>
                   </Link>
                   <p>
-                    <span>{tour.price.toLocaleString()}</span> تومان
+                    <span>{tour.price.toLocaleString("fa")}</span> تومان
                   </p>
                 </div>
               </div>
